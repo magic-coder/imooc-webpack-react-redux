37ee063b7bccb11272fe88de15e27d3a4ee2f6c0
Li Li <allen.lili@hotmail.com>
2018-05-19 16:54:58 +0800

    组件间的数据传递by props

--- 01-knowledge-base/imooc-react-app/src/App.js
+++ 01-knowledge-base/imooc-react-app/src/App.js
@@ -6,16 +6,20 @@ class App extends React.Component {
     return (
       <div>
         <h2>独立团{boss}</h2>
-        <YiYing />
+        <YiYing laoda='ZhangDaMiao' />
+        <QiBingLian laoda='ZhangDaMiao' />
       </div>
     );
   }
 }
 
+function QiBingLian(props) {
+  return <h2>QiBingLianLianZhang{props.laoda}, chonga!</h2>
+}
+
 class YiYing extends React.Component {
   render() {
-    const boss = 'ZhangDaMiao';
-    return <h2>YiYingYingZhang, {boss}</h2>;
+    return <h2>YiYingYingZhang, {this.props.laoda}</h2>;
   };
 }
 
