2e2300c339ba263248833676d3d8b21cdbae91de
Li Li <allen.lili@hotmail.com>
2018-05-19 16:45:41 +0800

    basic example

--- 01-knowledge-base/imooc-react-app/src/App.js
+++ 01-knowledge-base/imooc-react-app/src/App.js
@@ -1,21 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
   render() {
+    const boss = '李云龙';
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <h2>独立团{boss}</h2>
+        <YiYing />
       </div>
     );
   }
+}
+
+class YiYing extends React.Component {
+  render() {
+    const boss = 'ZhangDaMiao';
+    return <h2>YiYingYingZhang, {boss}</h2>;
+  };
 }
 
 export default App;
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 
-[https://github.com/shengxinjing/react-chat/tree/master/01_learn/02_es6](https://github.com/shengxinjing/react-chat/tree/master/01_learn/02_es6)
+[https://github.com/shengxinjing/react-chat/tree/master](https://github.com/shengxinjing/react-chat/tree/master)
