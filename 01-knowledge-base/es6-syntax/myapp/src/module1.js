5c81f06aa8cd39d138b3567c06babf263b9cf087
Li Li <allen.lili@hotmail.com>
2018-05-22 13:18:45 +0800

    brrename folders

--- 01-knowledge-base/es6-syntax/myapp/src/module1.js
+++
@@ -1,5 +0,0 @@
-export let name = 'imooc123';
-
-export default function sayHello() {
- console.log('imooc rocks!');
-}
--- 01-knowledge-base/imooc-react-app/src/redux/reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import { userReducer } from '../redux/auth.redux';
-import { gunReducer } from '../redux/dashboard.redux';
-
-const auth = userReducer;
-const counter = gunReducer;
-export default combineReducers({auth, counter});
